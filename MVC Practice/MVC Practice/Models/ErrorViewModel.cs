c8937179553528d69daf4ecd23775c647d675aba
Ananya <afmtrendaid@gmail.com>
2023-07-31 10:48:22 -0700

    mother

---
+++ MVC Practice/MVC Practice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_Practice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
