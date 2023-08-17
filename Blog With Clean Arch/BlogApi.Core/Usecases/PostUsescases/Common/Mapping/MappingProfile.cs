94aa1c82cdeb1a6ec5272380c5590ff3bfa07c7e
Ananya <afmtrendaid@gmail.com>
2023-08-17 00:25:14 -0700

    Mapping and Dtos

---
+++ Blog With Clean Arch/BlogApi.Core/Usecases/PostUsescases/Common/Mapping/MappingProfile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApi.Core.Usecases.PostUsescases.Common.Mapping
+{
+    internal class MappingProfile
+    {
+    }
+}
